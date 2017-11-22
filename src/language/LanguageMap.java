40454b0737ccfcd32c7c986d105addbe07e0a31c
mrowland-seu <mrowland-seu@users.noreply.github.com>
2017-11-22 00:58:07 -0700

    Update LanguageMap.java

--- src/language/LanguageMap.java
+++ src/language/LanguageMap.java
@@ -9,28 +9,47 @@ import java.util.Map;
 
 public class LanguageMap {
     private Map<String, String> langMap;
+    private Map<String, String> varMap;
 
-    public LanguageMap () {
+    public LanguageMap() {
         langMap = new HashMap<>();
+        varMap = new HashMap<>();
 
         langMap.put("forward", "add");
         langMap.put("back" , "subtract");
         langMap.put("jump", "multiply");
         langMap.put("crouch", "divide");
+        langMap.put("zerg", "power");
         langMap.put("spawn", "print");
         langMap.put("$", "comment");
+        langMap.put("/$", "endComment");
+        langMap.put("newPlayer", "newVar");
+        langMap.put("changePlayer" , "updateVar");
+        langMap.put("levelUp", "power");
     }
 
     public String getValueForMapKey(String key) {
-        return langMap.get(key);
+        if (languageMapContainsKey(key))
+            return langMap.get(key);
+        if (variableMapContainsKey(key))
+            return varMap.get(key);
+        throw new IllegalArgumentException("");
     }
 
-    public void addVariableToMap (String key, String value) {
-        langMap.put(key, value);
+    public void addToVariableMap(String key, String value) {
+        varMap.put(key, value);
     }
 
-    public void updateVariableInMap (String key, String value) {
-        langMap.replace(key, value);
+    public void updateVariableInVariableMap(String key, String value) {
+        varMap.replace(key, value);
+    }
+
+    public boolean languageMapContainsKey(String key) {
+        return langMap.containsKey(key);
+    }
+
+    public boolean variableMapContainsKey(String key) {
+        return varMap.containsKey(key);
     }
 
 }
